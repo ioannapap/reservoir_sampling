866891bfa8b9628c4dd4dc9c466e142edcc8fcaf
Ioanna Papagianni <ioannapap@users.noreply.github.com>
2019-11-09 18:16:16 +0200

    Add files via upload

--- reservoir.py
+++ reservoir.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 		while row != ' ':
 
 			sampled_rows = reservoir_sampling(K, row, row_number, sampled_rows)
-			print(sampled_rows)
+			#print(sampled_rows)
 
 			try:
 
